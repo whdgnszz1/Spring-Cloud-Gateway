aa6968893ea70793e4bc7c8541a72255987c823c
홍종훈 <whdgnszz1@snu.ac.kr>
2024-07-05 10:27:54 +0900

    Feat: API Gateway에 JWT 필터 적용

--- src/main/java/com/jh/apigatewayservice/filter/CustomFilter.java
+++ src/main/java/com/jh/apigatewayservice/filter/CustomFilter.java
@@ -1,17 +1,27 @@
 package com.jh.apigatewayservice.filter;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
+import java.util.Map;
+
 @Component
 @Slf4j
 public class CustomFilter extends AbstractGatewayFilterFactory<CustomFilter.Config> {
+
+    @Value("${jwt.secret.key}")
+    private String jwtSecret;
 
     public CustomFilter() {
         super(Config.class);
@@ -19,14 +29,43 @@ public class CustomFilter extends AbstractGatewayFilterFactory<CustomFilter.Conf
 
     @Override
     public GatewayFilter apply(Config config) {
-        // Custom Pre Filter. Suppose we can extract JWT and perform Authentication
         return (exchange, chain) -> {
             ServerHttpRequest request = exchange.getRequest();
             ServerHttpResponse response = exchange.getResponse();
 
+            if (!request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                response.setStatusCode(HttpStatus.UNAUTHORIZED);
+                return response.setComplete();
+            }
+
+            String authHeader = request.getHeaders().getOrEmpty(HttpHeaders.AUTHORIZATION).get(0);
+            if (!authHeader.startsWith("Bearer ")) {
+                response.setStatusCode(HttpStatus.UNAUTHORIZED);
+                return response.setComplete();
+            }
+
+            String token = authHeader.substring(7);
+
+            try {
+                Claims claims = Jwts.parser()
+                        .setSigningKey(jwtSecret)
+                        .parseClaimsJws(token)
+                        .getBody();
+
+                log.info("JWT Claims: ");
+                for (Map.Entry<String, Object> entry : claims.entrySet()) {
+                    log.info("{}: {}", entry.getKey(), entry.getValue());
+                }
+
+            } catch (Exception e) {
+                log.error("JWT validation failed", e);
+                response.setStatusCode(HttpStatus.UNAUTHORIZED);
+                return response.setComplete();
+            }
+
             log.info("Custom PRE filter: request uri -> {}", request.getURI());
             log.info("Custom PRE filter: request id -> {}", request.getId());
-            // Custom Post Filter. Suppose we can call error response handler based on error code.
+
             return chain.filter(exchange).then(Mono.fromRunnable(() -> {
                 log.info("Custom POST filter: response status code -> {}", response.getStatusCode());
             }));
@@ -35,8 +74,6 @@ public class CustomFilter extends AbstractGatewayFilterFactory<CustomFilter.Conf
 
     @Data
     public static class Config {
-        // Put the configuration properties for your filter here
-        // For example, you can add a boolean to enable/disable logging
         private boolean preLogger;
         private boolean postLogger;
     }
